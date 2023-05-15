4b53361968d612c372b79559f4fa3998bceee85f
Paolo <59060818+absolutelyrelative@users.noreply.github.com>
2023-05-15 09:57:15 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -3,5 +3,18 @@ DROP TABLE IF EXISTS video;
 CREATE TABLE video (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     shorturl TEXT UNIQUE NOT NULL,
-    loc TEXT UNIQUE NOT NULL
+    loc TEXT UNIQUE NOT NULL,
+    downloaded BOOLEAN DEFAULT 0
 );
+
+CREATE TABLE collection (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    shorturl TEXT UNIQUE, --Collections are treated locally to nullify same video redownload, therefore shorturl can be null
+);
+
+CREATE TABLE videocollectionmembership (
+    videoid INTEGER NOT NULL,
+    collectionid INTEGER NOT NULL,
+    FOREIGN KEY (videoid) REFERENCES video(id),
+    FOREIGN KEY (collectionid) REFERENCES collection(id)
+)
