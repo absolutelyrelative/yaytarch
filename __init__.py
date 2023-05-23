08a0bc62ddc2a5365b41eede660d07aa39b6495a
Paolo <59060818+absolutelyrelative@users.noreply.github.com>
2023-05-23 21:13:00 +0200

    Further code sanitisation

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 import os
 from flask import Flask
 from . import db, collections, videos
-from yt_dlp import YoutubeDL
 import click
 from yaytarch.db import get_db
 import json
@@ -19,63 +18,9 @@ def create_app(test_config=None):
         pass
 
     db.init_app(app)
-    app.register_blueprint(collections.bp)
-    app.register_blueprint(videos.bp)
-    app.add_url_rule('/', endpoint='index')
-
-    @app.cli.command("dl")
-    @click.argument("link")
-    def dl(link):
-        coolkeys = ['id', 'title', 'thumbnail', 'description', 'release_date', 'format', 'format_id', 'ext', 'width', 'height', 'resolution']
-
-        locdict = {
-            'home' : os.getcwd() + '\\videos\\'
-        }
-        output_template_dic = {
-            'default' : "%(id)s.%(ext)s"
-        }
-        ytdlp_options = {'paths' : locdict, 'outtmpl' : output_template_dic, 'format': 'mp4'}
-        with YoutubeDL(ytdlp_options) as ydl:
-            info = ydl.extract_info(link, download=False)
-            dict_dump = ydl.sanitize_info(info)
-            subdct = {key: dict_dump[key] for key in coolkeys}
-            ydl.download(link)
-            registervideo(subdct, locdict)
     
-    def registervideo(dict, locdict):
-        db = get_db()
-        loc = locdict['home'] + dict['id'] + '.' + dict['ext']
-        print('location: ' + loc)
-
-        try:
-            db.execute(
-                "INSERT INTO video (shorturl, loc, downloaded, title, width, height, descr, resolution) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-                (dict['id'], loc, 1, dict['title'], dict['width'], dict['height'], dict['description'], dict['resolution']),
-            )
-            db.commit()
-        except db.IntegrityError:
-            print("oops")
-        else:
-            pass
-
-        #TODO: Pass into its own generalised function with automatic ID detection
-        # this is just to make testing easier for the moment
-        
-        #Add to 'All' category automatically
-        print(dict['id'])
-        video = db.execute('SELECT id FROM video WHERE video.shorturl = "' + dict['id'] + '"'
-        ).fetchone()
-        videokey = video['id']
-        try:
-            db.execute(
-                "INSERT INTO videocollectionmembership (videoid, collectionid) VALUES (?, ?)",
-                (videokey,1),
-            )
-            db.commit()
-        except db.IntegrityError:
-            print("oops")
-        else:
-            pass
-
+    app.register_blueprint(collections.bp)
+    app.register_blueprint(videos.bp, cli_group=None)
+    app.add_url_rule('/', endpoint='index')
     
     return app
--- videos.py
+++ videos.py
@@ -1,4 +1,5 @@
-import functools
+import functools, click
+from yt_dlp import YoutubeDL
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for, send_from_directory
@@ -31,3 +32,59 @@ def load_video(video_id):
     dir_name = os.path.dirname(location_from_db)
 
     return send_from_directory(dir_name, file_name)
+
+
+@bp.cli.command("dl")
+@click.argument("link")
+def dl(link):
+    coolkeys = ['id', 'title', 'thumbnail', 'description', 'format', 'format_id', 'ext', 'width', 'height', 'resolution']
+
+    locdict = {
+        'home' : os.getcwd() + '\\videos\\'
+    }
+    output_template_dic = {
+        'default' : "%(id)s.%(ext)s"
+    }
+    ytdlp_options = {'paths' : locdict, 'outtmpl' : output_template_dic, 'format': 'mp4'}
+    with YoutubeDL(ytdlp_options) as ydl:
+        info = ydl.extract_info(link, download=False)
+        dict_dump = ydl.sanitize_info(info)
+        #TODO: Investigate why 'release_date' in coolkeys throws error in dict. Maybe it is missing?
+        subdct = {key: dict_dump[key] for key in coolkeys}
+        ydl.download(link)
+        registervideo(subdct, locdict)
+    
+def registervideo(dict, locdict):
+    db = get_db()
+    loc = locdict['home'] + dict['id'] + '.' + dict['ext']
+    print('location: ' + loc)
+
+    try:
+        db.execute(
+            "INSERT INTO video (shorturl, loc, downloaded, title, width, height, descr, resolution) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+            (dict['id'], loc, 1, dict['title'], dict['width'], dict['height'], dict['description'], dict['resolution']),
+        )
+        db.commit()
+    except db.IntegrityError:
+        print("oops")
+    else:
+        pass
+
+    #TODO: Pass into its own generalised function with automatic ID detection
+    # this is just to make testing easier for the moment
+    
+    #Add to 'All' category automatically
+    print(dict['id'])
+    video = db.execute('SELECT id FROM video WHERE video.shorturl = "' + dict['id'] + '"'
+    ).fetchone()
+    videokey = video['id']
+    try:
+        db.execute(
+            "INSERT INTO videocollectionmembership (videoid, collectionid) VALUES (?, ?)",
+            (videokey,1),
+        )
+        db.commit()
+    except db.IntegrityError:
+        print("oops")
+    else:
+        pass
