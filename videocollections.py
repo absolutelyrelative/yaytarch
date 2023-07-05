00fafc4a9695521b699be011254b59873b133a62
Paolo <59060818+absolutelyrelative@users.noreply.github.com>
2023-07-05 10:29:50 +0200

    Implemented video collection image fetch

--- videocollections.py
+++ videocollections.py
@@ -1,3 +1,5 @@
+import os.path
+
 from tools.outputformat import bcolors
 from .model import collectionmodel
 from .model import collectionmodel
@@ -5,9 +7,8 @@ from .model import videocollectionrelmodel as videocollectionmembershipmodel
 from .model import videomodel
 from .tools import videodownload
 from flask import (
-    Blueprint, render_template, request
+    Blueprint, render_template, request, send_from_directory
 )
-
 
 # This blueprint shows all the available collections and any future feature related to collections
 
@@ -43,35 +44,46 @@ def index():
             collectionobj = collectionmodel.findcollectionbyname(collectionname)  # Fetch collection object from name
             if collectionobj is not None:
                 # fetch videos from collection
-                videolist = videocollectionmembershipmodel.getvideocollectionmembershipbyid(collectionobj.id, "COLLECTION")
+                videolist = videocollectionmembershipmodel.getvideocollectionmembershipbyid(collectionobj.id,
+                                                                                            "COLLECTION")
                 # attempt to move videos to "All Videos" playlist. Done outside the loop to save some computational power.
                 allvideoscollection = collectionmodel.findcollectionbyname("All videos")
                 for video in videolist:
                     videomodel.addvideotocollection(video.id, allvideoscollection.id)
 
                 # attempt to remove all collection, video relationships and the collection entry itself
-                if videocollectionmembershipmodel.removeallcollectionentries(collectionobj.id) is not None and collectionmodel.removecollection(collectionobj.id) is not None:
+                if videocollectionmembershipmodel.removeallcollectionentries(
+                        collectionobj.id) is not None and collectionmodel.removecollection(
+                        collectionobj.id) is not None:
                     print(bcolors.OKGREEN + "Collection removed." + bcolors.ENDC)
                 else:
-                    print(bcolors.WARNING + "Could not delete collection. Check terminal for more information." + bcolors.ENDC)
-
-
+                    print(
+                        bcolors.WARNING + "Could not delete collection. Check terminal for more information." + bcolors.ENDC)
 
     collections = collectionmodel.getallcollections()
     return render_template('collections.html', collections=collections)
 
 
-#TODO: Refactor with a more consistent argument passing
+# TODO: Refactor with a more consistent argument passing
 @bp.route('/collection/<int:collectionid>', methods=['GET', 'POST'])
 def viewcollection(collectionid):
     if request.method == 'POST':
         try:
             collectionname = collectionmodel.getvideocollectionbyid(collectionid).vcname
         except:
-            print("Collection not found") #Temporary solution
+            print("Collection not found")  # Temporary solution
         if 'videourl' in request.form.keys():
             videourl = request.form['videourl']
             videodownload.dl(videourl, collectionname)
 
     videos = videocollectionmembershipmodel.getvideocollectionmembershipbyid(collectionid, type="COLLECTION")
     return render_template('videolist.html', videos=videos)
+
+
+@bp.route("/collection/source/thumb/<int:collectionid>")
+def load_picture(collectionid):
+    collection = collectionmodel.getvideocollectionbyid(collectionid)
+    dirname = os.path.dirname(collection.thumbnail)
+    filename = os.path.basename(collection.thumbnail)
+
+    return send_from_directory(dirname, filename)
