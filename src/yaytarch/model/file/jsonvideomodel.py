2a044c66bc90f0bc5512447eebae00c6185f3cc3
Paolo <59060818+absolutelyrelative@users.noreply.github.com>
2023-08-25 19:13:21 +0200

    Test commit

--- src/yaytarch/model/file/jsonvideomodel.py
+++ src/yaytarch/model/file/jsonvideomodel.py
@@ -3,6 +3,19 @@
 # The difference between the other videomodel is that this will fetch information from json file and probably be more
 # flexible. The vision is to also make it more extensible should requested tags to download change.
 import json
+
+# Filter for unwanted keys. TODO: Ideally, this should be fetched by configuration.
+def keyfilter(pair):
+    filters = ['title']
+
+    key, value = pair
+    for unwanted in filters:
+        if key == unwanted:
+            return False
+        else:
+            return True
+
+
 class video:
 
     def __init__(self, **kwargs):
@@ -18,10 +31,15 @@ class video:
 # Returns None if generation failed.
 def generatevideofromjsonfile(filename: str) -> video:
     keys = ['id', 'title']  # important keys that MUST be present in the json
-    filter = [] # TODO: Implement filter to not bloat the object. Ideally, this should be fetched by configuration.
+
     if filename is not None and filename != '':  # if json file is found, extract info
         with open(filename) as jsonfile:
             data = json.load(jsonfile)
+
+            # Remove unwanted keys. Necessary because jsons can get big.
+            # A performance study should be done to see if it's worth it.
+            # TODO: This is a horrible approach and may destroy performance
+            data = dict(filter(keyfilter, data.items()))
 
             # TODO: is this the right approach?
             for key in keys:
